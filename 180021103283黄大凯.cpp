5289b603d35548c13505533c7d0e2f73575bace2
kaiakz <kaiakx@yandex.com>
2019-12-01 19:50:40 +0800

    Homework 3

--- 180021103283黄大凯.cpp
+++ 180021103283黄大凯.cpp
@@ -1,40 +1,84 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-static const int N = 100;
+static const int MAX = 100;
+static const int INFTY = (1<<21);
+static const int WHITE = 0;
+static const int GRAY = 1;
+static const int BLACK = 2;
+
+int n, M[MAX][MAX];
+
+void dijkstra() {
+    int minv;
+    int d[MAX], color[MAX];
+
+    for (int i = 0; i < n; i++)
+    {
+        d[i] = INFTY;
+        color[i] = WHITE;
+    }
+    
+    d[0] = 0;
+    color[0] = GRAY;
+    while (1)
+    {
+        minv = INFTY;
+        int u = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (minv > d[i] && color[i] != BLACK)
+            {
+                u = i;
+                minv = d[i];
+            }
+        }
+        if (u == -1)    break;
+        color[u] = BLACK;
+        for (int v = 0; v < n; v++)
+        {
+            if (color[v] != BLACK && M[u][v] != INFTY)
+            {
+                if (d[v] > d[u] + M[u][v])
+                {
+                    d[v] = d[u] + M[u][v];
+                    color[v] = GRAY;
+                }   
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " " << (d[i] == INFTY ? -1 : d[i]) << endl;
+    }
+    
+}
 
 int main(int argc, char const *argv[])
 {
-    int n, p[N+1], m[N+1][N+1];
     cin >> n;
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin >> p[i-1] >> p[i];
-    }
-    
-
-
-    for (int i = 1; i <= n; i++)
-    {
-        m[i][i] = 0;
-    }
-    for (int l = 2; l <= n; l++)
-    {
-        for (int i = 1; i <= n - l + 1; i++)
+        for (int j = 0; j < n; j++)
         {
-            int j = i + l - i;
-            m[i][j] = (1 << 21);
-            for (int k = i; k <= j-1; k++)
-            {
-                m[i][j] = min(m[i][j], m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j]);
-            }
-        }        
+            M[i][j] = INFTY;
+        }
     }
     
-
-    cout << m[1][n] << endl;
+    int k, c, u, v;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> u >> k;
+        for (int j = 0; j < k; j++)
+        {
+            cin >> v >> c;
+            M[u][v] = c;
+        }
+    }
+    dijkstra();
 
     return 0;
+    
 }
